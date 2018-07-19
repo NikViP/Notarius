668e1c83c7b743a459c33f96bb0e7a6aca792f96
NikViP <p11591@mail.ru>
2018-07-19 14:49:50 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.querySelector('.invitation-modal').classList.toggle('invitation-modal-show');
+
+document.querySelector('.button-invitation')
+    .addEventListener('click', function () {
+      document.querySelector('.invitation-modal').classList.toggle('invitation-modal-show')
+    });
+
+
