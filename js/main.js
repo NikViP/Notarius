f23081ff706df46b7534d5f57557357acb870344
NikViP <p11591@mail.ru>
2018-08-19 11:30:16 +0500

    Добавил тексты документов. Добавил скрипт для плавной прокрутки страницы до якоря

--- js/main.js
+++ js/main.js
@@ -8,4 +8,25 @@
 //  tick();
 //}, 5000);
 
+01
+$(document).ready(function(){
+  02
+  $('.navigation-list a').click(function(){
+    03
+    /*задали какой мы хотим отступ от верха страницы*/
+    04
+    var otstupTop=100;
+    05
+    $('body,html').animate({
+      06
+      /*получили положение элемента вычли отступ и прокрутили*/
+          07
+      scrollTop: $($(this).attr('href')).offset().top-otstupTop
+      08
+    }, 1500);
+    09
+  });
+  10
+});
+
 
